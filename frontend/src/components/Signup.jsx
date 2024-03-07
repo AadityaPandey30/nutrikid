59a9024a95d85d1ec2c10c1e17034865c397c68c
Aaditya Pandey <aprock3004@gmail.com>
2024-03-07 12:22:33 +0530

    Signup Changed

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
@@ -7,6 +7,24 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [passwordConfirm, setpasswordConfirm] = useState("");
   const navigate = useNavigate();
+const Signup = () =>{
+    return(
+        <div className="container">
+            <div className="login p-5 w-[50%] min-w-[320px] m-auto mt-[10vh] rounded-[10px] text-center">
+                <h1 className="text-3xl mb-8 font-semibold">SignUp Here</h1>
+                <input type="text" className="w-full mb-6 h-8 px-3 py-6 rounded-[10px] bg-yellow-50 text-yellow-700" placeholder="Enter UserName"/>
+                <input type="email" className="w-full mb-6 h-8 px-3 py-6 rounded-[10px] bg-yellow-50 text-yellow-700" placeholder="Enter Email"/>
+                <input type="password" className="w-full mb-6 h-8 px-3 py-6 rounded-[10px] bg-yellow-50 text-yellow-700" placeholder="Enter Password"/>
+                <input type="password" className="w-full mb-6 h-8 px-3 py-6 rounded-[10px] bg-yellow-50 text-yellow-700" placeholder="Confirm Password"/>
+                <div className="text-right">
+                <button className="rounded-[8px] px-7 py-2 bg-[#F5BF26] font-bold text-black">SignUp</button>
+                </div>
+                <p className="py-4 px-1 text-center">Already have an account? <a href="./login" className="underline text-yellow-700">Login</a> here</p>
+            </div>
+        </div>
+    )
+}
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -67,6 +85,18 @@ const Signup = () => {
             placeholder="Confirm Password"
             required
           />
+          <h1 className="text-1xl pt-4 pb-2 text-right" >Enter some details of your Child</h1>
+          <div className="text-right">
+                    <input type="text" placeholder="Height(cm)" className="bg-[#ffde85] rounded-[4px] py-2 w-[95px] text-center px-1 mx-2"/>
+                    <input type="text" placeholder="Weight(cm)" className="bg-[#ffde85] rounded-[4px] py-2 w-[95px] text-center px-1 mx-2"/>
+                    <select name="age" id="age" className='px-5 py-2.5 md:mt-3 mt-1 md:mb-4 mb-1 ml-2' required >
+                        <option value="None" selected>Age</option>
+                        <option value="1-3">1-3</option>
+                        <option value="3-5">3-5</option>
+                        <option value="5-7">5-7</option>
+                        <option value="8">8</option>
+                    </select>
+                </div>
           <div className="text-right">
             <button
               type="submit"
