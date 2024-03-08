e196706043029e695bcba4ce0f541a25fafbbe3d
Priyanshu Bisht <priyanshubisht451@gmail.com>
2024-03-09 05:26:56 +0530

    changes

--- backend/controllers/dietController.js
+++ backend/controllers/dietController.js
@@ -1,12 +1,38 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const axios = require("axios");
 
-exports.getDiets = catchAsync(async (req, res, next) => {});
+exports.getDiets = catchAsync(async (req, res, next) => {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const generationConfig = {
+    temperature: 0.7,
+    topP: 1,
+    topK: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    generationConfig,
+  });
+
+  const prompt = req.body.prompt;
+  const result = await model.generateContent(prompt);
+  const data = await result.response;
+
+  if (!data) {
+    return next(new AppError("Error generating response!", 401));
+  }
+  console.log(data);
+  res.status(200).json({
+    status: "success",
+    data: data.candidates[0].content.parts[0].text,
+  });
+});
 
 //https://cosylab.iiitd.edu.in/rdbapi/recipeDB/searchRecipeByNutrients?
 //energy=328:330&carbohydrates=5:7&protien=52:53&calories=455:460&fat=10:12
-
 
 exports.showRecipe = catchAsync(async (req, res, next) => {
   const recipeId = req.params.recipeId;
--- backend/routes/dietRouter.js
+++ backend/routes/dietRouter.js
@@ -8,7 +8,7 @@ router.use(authController.protect);
 
 router.route("/recipe/:recipeId").get(dietController.showRecipe);
 
-router.route("/diets/:issueId").get(dietController.getDiets); //in the issueId pass name of the issue
+router.route("/diets/").get(dietController.getDiets); //in the issueId pass name of the issue
 //we're only convering fever and loose motions for now, so only routes available are fever, loosemotion
 
 module.exports = router;
